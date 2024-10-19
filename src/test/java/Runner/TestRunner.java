4be62b93cbb6507bd919e419fba7930fe618f5d6
nithi guna <nithimadhavguna@gmail.com>
2024-10-19 06:42:28 -0500

    Runner file update with DAvid feature

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -8,14 +8,18 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import utilities.ConfigFileReader;
 
-@CucumberOptions(features = { "./src/test/resources/davidfeature" }, // feature files path
-		glue = { "davidStepDefinitions", "david_hooks" }, // step definition and hooks package
-		monochrome = true,
-				tags= "@Login or @RegisterInvalidCredentials or @LinkedList or @Stack or @Tree or @DataStructure or @Queue or @Graph or @Array" , 
-		plugin = {
-				"pretty", "html:target/cucumber-reports/cucumber.html",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm" })
+@CucumberOptions(
+		 features = {"./src/test/resources/davidfeature"},
+   glue = {"davidStepDefinitions","david_hooks"},
+   monochrome = true,
+
+   tags= "@Login or @RegisterInvalidCredentials or @LinkedList or "
+   		+ "@Stack or @Tree or @DataStructure or @Queue or @Graph or @Array",
+   plugin= {"pretty","html:target/cucumber-reports/cucumber.html",
+   		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+   		 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+   		// "rerun:target/rerun.txt",
+   		})
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
